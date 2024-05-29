bdee4d399e5376b6824ad1342383a2874598b661
sabrinabuenof <127441722+sabrinabuenof@users.noreply.github.com>
2024-05-29 19:07:55 -0300

    async await

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if(r.erro) {
-        throw Error ('Esse cep não existe!')
-    } else     
-    console.log(r)})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluido!'));
+async function buscaEndereco () {
+    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+    var consultaCEPConvertida = await consultaCEP.json()
+    console.log(consultaCEPConvertida);
+}
 
-console.log(consultaCEP);
+
+
+buscaEndereco ();
