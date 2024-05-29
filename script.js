474e260342ba31785af213f180e806699e788f9c
sabrinabuenof <127441722+sabrinabuenof@users.noreply.github.com>
2024-05-28 21:59:10 -0300

    finally

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
 .then(resposta => resposta.json())
 .then(r => {
     if(r.erro) {
         throw Error ('Esse cep não existe!')
     } else     
     console.log(r)})
-.catch(erro => console.log(erro));
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento concluido!'));
 
 console.log(consultaCEP);
