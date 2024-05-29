5a6d9cb1e9fbe212cb0643344279d491b9374b2c
sabrinabuenof <127441722+sabrinabuenof@users.noreply.github.com>
2024-05-29 20:53:52 -0300

    preenchimento automático

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ async function buscaEndereco (cep) {
     if (consultaCEPConvertida.erro) {
         throw Error ('CEP não existente!');
     }
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+
     console.log(consultaCEPConvertida);
     return consultaCEPConvertida;
 }catch (erro) {
     console.log(erro)
     }   
 }
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
 
