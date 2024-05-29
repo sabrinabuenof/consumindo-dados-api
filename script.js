f0121b4daad556420778692ebb8272ab9d2162c0
sabrinabuenof <127441722+sabrinabuenof@users.noreply.github.com>
2024-05-28 21:06:22 -0300

    tratando erros com catch

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json()
-.then(r => console.log(r)));
+var consultaCEP = fetch('https://viacep.com.br/ws/01001250/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if(r.erro) {
+        throw Error ('Esse cep não existe!')
+    } else     
+    console.log(r)})
+.catch(erro => console.log(erro));
 
 console.log(consultaCEP);
